c8972fa290d3a205f63a7288b7f553bcd193dba6
MirrorMind <mirrormind@outlook.com>
2015-01-19 09:37:08 +1100

    Added formatting preferences and readme

---
+++ mo-test-eam/src/moeam/dbScripts/DbConnectionModule.java
@@ -0,0 +1,5 @@
+package moeam.dbScripts;
+
+public class DbConnectionModule {
+
+}
--- mo-test-eam/src/moeam/dbScripts/ExecuteAllScripts.java
+++ mo-test-eam/src/moeam/dbScripts/ExecuteAllScripts.java
@@ -12,59 +12,67 @@ import java.util.ArrayList;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.Test;
 
-public class ExecuteAllScripts {
+@Deprecated
+public class ExecuteAllScripts
+{
 
-	private final String DB_URL = "jdbc:mysql://localhost:3306/test";
-	private final String DB_USERNAME = "root";
-	private final String DB_PASSWORD = "01001";
+    private final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    private final String DB_USERNAME = "root";
+    private final String DB_PASSWORD = "01001";
 
-	/**
-	 * Run this test to create all tables for the database
-	 * 
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
-	 */
-	@Test
-	public void executeAllScripts() throws ClassNotFoundException, SQLException {
-		// Get driver to connect to the DB
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME,
-				DB_PASSWORD);
+    /**
+     * Run this test to create all tables for the database
+     * 
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    @Test
+    public void executeAllScripts() throws ClassNotFoundException, SQLException {
+        // Get driver to connect to the DB
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME,
+                DB_PASSWORD);
 
-		// Get all script names and execute each script
-		ArrayList<String> scripts = getScriptNames();
-		for (String script : scripts) {
+        // Get all script names and execute each script
+        ArrayList<String> scripts = getScriptNames();
+        for (String script : scripts)
+        {
 
-			// Try read a script and execute it
-			try {
-				ScriptRunner scriptRunner = new ScriptRunner(conn);
-				BufferedReader reader = new BufferedReader(new FileReader(
-						script));
-				scriptRunner.runScript(reader);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+            // Try read a script and execute it
+            try
+            {
+                ScriptRunner scriptRunner = new ScriptRunner(conn);
+                BufferedReader reader = new BufferedReader(new FileReader(
+                        script));
+                scriptRunner.runScript(reader);
+            }
+            catch (FileNotFoundException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
-	// TODO directory.listFiles() returns null so this class doesn't actually work yet
-	/** Get all script names in this package */
-	private ArrayList<String> getScriptNames() {
-		Class<ExecuteAllScripts> thisClass = ExecuteAllScripts.class;
-		Package thisPackage = thisClass.getPackage();
-		String packageName = thisPackage.getName();
+    // TODO directory.listFiles() returns null so this class doesn't actually
+    // work yet
+    /** Get all script names in this package */
+    private ArrayList<String> getScriptNames() {
+        Class<ExecuteAllScripts> thisClass = ExecuteAllScripts.class;
+        Package thisPackage = thisClass.getPackage();
+        String packageName = thisPackage.getName();
 
-		// Locate all files in this package directory
-		File directory = new File(packageName);
-		File[] files = directory.listFiles();
+        // Locate all files in this package directory
+        File directory = new File(packageName);
+        File[] files = directory.listFiles();
 
-		// Retrieve class names
-		ArrayList<String> classNames = new ArrayList<String>();
-		for (File file : files) {
-			if (file.getName().endsWith(".sql"))
-				classNames.add(file.getName());
-		}
-		return classNames;
-	}
+        // Retrieve class names
+        ArrayList<String> classNames = new ArrayList<String>();
+        for (File file : files)
+        {
+            if (file.getName().endsWith(".sql"))
+                classNames.add(file.getName());
+        }
+        return classNames;
+    }
 }
